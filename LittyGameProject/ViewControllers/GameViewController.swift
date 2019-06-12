4ac0d725be21f3483398b8bdb384cd54a655b8a5
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-06-12 15:48:51 -0600

    test commit

--- LittyGameProject/ViewControllers/GameViewController.swift
+++ LittyGameProject/ViewControllers/GameViewController.swift
@@ -12,7 +12,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let goldfish = 6
+        
         
         // Do any additional setup after loading the view.
     }

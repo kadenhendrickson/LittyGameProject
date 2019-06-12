c0acd226d842973f889080d6a309b761c0aa84eb
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-06-12 15:46:45 -0600

    test commit

---
+++ LittyGameProject/ViewControllers/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  LittyGameProject
+//
+//  Created by Kaden Hendrickson on 6/12/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let goldfish = 6
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

86de74f52a255df3570b48a6ef1d8e10598629b9
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-06-13 16:18:22 -0600

    Add Settings View Controller

--- LittyGameProject/ViewControllers/SettingsViewController.swift
+++ LittyGameProject/ViewControllers/SettingsViewController.swift
@@ -10,21 +10,60 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    static let shared = SettingsViewController()
+    
+    var cardBackColor: UIImage = UIImage(named: "redCardBack") ?? UIImage()
+    var shuffleSpeed: TimeInterval = 0.75
+    
+    @IBOutlet weak var blueCardButton: BlueMonteButton!
+    @IBOutlet weak var greenCardButton: GreenMonteButton!
+    @IBOutlet weak var yellowCardButton: YellowMonteButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func backButtonTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeColorButtonsTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            SettingsViewController.shared.cardBackColor = UIImage(named: "blueCardBack") ?? UIImage()
+            self.navigationController?.popViewController(animated: true)
+            print("card color changed to blue")
+        case 2:
+            SettingsViewController.shared.cardBackColor = UIImage(named: "greenCardBack") ?? UIImage()
+            self.navigationController?.popViewController(animated: true)
+            print("card color changed to green")
+        case 3:
+            SettingsViewController.shared.cardBackColor = UIImage(named: "yellowCardBack") ?? UIImage()
+            self.navigationController?.popViewController(animated: true)
+            print("card color changed to yellow")
+        default:
+            print("damn brother impressive")
+        }
     }
-    */
+    
+    @IBAction func difficultyButtonTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            SettingsViewController.shared.shuffleSpeed = 1.0
+            self.navigationController?.popViewController(animated: true)
+        case 2:
+            SettingsViewController.shared.shuffleSpeed = 0.75
+            self.navigationController?.popViewController(animated: true)
+        case 3:
+            SettingsViewController.shared.shuffleSpeed = 0.5
+            self.navigationController?.popViewController(animated: true)
+        case 4:
+            SettingsViewController.shared.shuffleSpeed = 0.25
+            self.navigationController?.popViewController(animated: true)
+        default:
+            print("this is broken if it hits this point")
+        }
+    }
+    
 
 }
